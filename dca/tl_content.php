6ab2de9b53dee8cb9a9c8bc1fe52dea7462ba91b
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2016-09-29 20:33:48 +0330

    Add responsive option

--- dca/tl_content.php
+++ dca/tl_content.php
@@ -14,7 +14,7 @@
  * Add palettes to tl_module
  */
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['aparat'] = '{type_legend},type,headline;{source_legend},aparat;{player_legend},playerSize;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['aparat'] = '{type_legend},type,headline;{source_legend},aparat;{player_legend},aparat_responsive,playerSize;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 
 /**
  * Add fields to tl_module
@@ -26,4 +26,12 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['aparat'] = array
 	'inputType'               => 'text',
 	'eval'                    => array('rgxp'=>'url', 'mandatory'=>true),
 	'sql'                     => "varchar(16) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields']['aparat_responsive'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_content']['aparat_responsive'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array(),
+	'sql'                     => "char(1) NOT NULL default ''"
 );
